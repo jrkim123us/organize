0c42e7cfb80da60df71e7ad399261d25da085966
Kim Jong Rok <jrkim123us@gmail.com>
2015-01-17 15:33:03 +0900

    최초 등록

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('organizeApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
